b341348ba5b6466ed28accb60afd4f890b1eb53c
Sid-003 <siddhartha4030@gmail.com>
2022-02-05 17:50:35 -0500

    Add scheduled queries

--- MapScraper.py
+++ MapScraper.py
@@ -3,19 +3,31 @@ from datetime import datetime
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import threading
 
 import MessageParser
 
 
 class MapScraper():
-    PATH = "C:\\Users\\Aniruthan Ramadoss\\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://ridebt.org/routes-schedules")
-    
-    def query_routes(self, text_message):
+    # PATH = "C:\\Users\\Aniruthan Ramadoss\\chromedriver.exe"
+    # driver = webdriver.Chrome(PATH)
+    # driver.get("https://ridebt.org/routes-schedules")
+
+    def schedule_query(self, text_message):
         query_info = MessageParser.parse_message(text_message)
-        datetime.now().strftime('%X') == text_message[3]
+        parsed = [int(x) for x in query_info[3].split(':')]
+        query_time = datetime.today().replace(hour=parsed[0], minute=parsed[1], second=0)
+        delta = query_time - datetime.now()
+
+        print(f'delta: {delta.seconds}')
+        timer = threading.Timer(delta.seconds, self.query_routes, [text_message])
+        timer.start()
+
+        print('Scheduled!!!')
+
+    def query_routes(self, text_message):
+        print(f'method called with args: {text_message}')
 
 
-
+scraper = MapScraper()
+scraper.schedule_query("TOM, Torgersen Hall, 1114, 17:58")
