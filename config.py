2cc01effe99f5db4d102ecb4bc323ec1bec0fb6c
Sid-003 <siddhartha4030@gmail.com>
2022-02-05 16:23:25 -0500

    Added Twilio interaction feature

--- config.py
+++
@@ -1,2 +0,0 @@
-account_SID = "AC9bae835c01312d973f229d84e7dfaa9b"
-auth_token = "4b7c98d0e63c7ffe61318576be20e810"
--- receive.py
+++ receive.py
@@ -1,19 +1,46 @@
-from flask import Flask, request, redirect
+import os
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio import twiml
+import config
+from twilio.rest import Client
+from pyngrok import ngrok
 
 app = Flask(__name__)
+client = Client(config.account_SID, config.auth_token)
 
 
-@app.route("/sms", methods=['GET', 'POST'])
-def sms():
-    number = request.form['From']
-    message_body = request.form['Body']
+@app.route('/bot', methods=['POST'])
+def bot():
+    user = request.values.get('From', '')
+    resp = MessagingResponse()
 
-    resp = twiml.Response()
-    print(number + ", " + message_body)
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    print(f"Got a message from: {user}")
+
+    print("\n\n Printing Form Values \n\n")
+    for value in request.form:
+        print(f"Type {type(value)} : {value}")
+    print("Message was: " + request.form["Body"])
+
+    return str(resp)
 
 
-if __name__ == "__main__":
+def send_message(number, msg):
+    client.messages.create(from_=config.phone_number,
+                           to=number,
+                           body=msg)
+
+
+def start_ngrok():
+    url = ngrok.connect(5000).public_url
+    print('*Tunnel URL:', url)
+
+    incoming_list = client.incoming_phone_numbers.list(phone_number=config.phone_number)
+    print(f"Length of list: {len(incoming_list)}")
+    for number in incoming_list:
+        number.update(sms_url=url + "/bot")
+        print(f"Number: {number.phone_number}")
+
+
+if __name__ == '__main__':
+    start_ngrok()
     app.run(debug=True)
